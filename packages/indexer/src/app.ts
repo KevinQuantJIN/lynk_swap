954e196a924fa656bf4e9cb2924514068738260c
Noy <59097122+noyyyy@users.noreply.github.com>
2024-06-03 06:44:19 +0800

    fix: add cors

--- packages/indexer/src/app.ts
+++ packages/indexer/src/app.ts
@@ -2,10 +2,15 @@ import fastify from 'fastify';
 import router from './router';
 import { startListen } from './task/listener';
 import JSONbig from 'json-bigint';
+import cors from '@fastify/cors';
 
 const server = fastify({
   // Logger only for production
   logger: !!(process.env.NODE_ENV !== 'development'),
+});
+
+server.register(cors, {
+  origin: '*',
 });
 
 server.addHook('preSerialization', (_request, _reply, payload: string, done) => {
